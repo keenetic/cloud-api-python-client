a4c8e0506a5d9919c9d248d4a9a0651cc65f2314
Dmitry Sivokhin <dmitry.sivokhin@keenetic.net>
2023-02-28 16:38:10 +0300

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ndcloudclient',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(),
     url='https://github.com/keenetic/cloud-api-python-client',
     license='',
