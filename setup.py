1fc89c2a6e44ad71848993976a9254d193f7decb
Dmitry Sivokhin <dmitry.sivokhin@keenetic.net>
2025-05-06 14:00:46 +0300

    update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ndcloudclient',
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(),
     url='https://github.com/keenetic/cloud-api-python-client',
     license='',
